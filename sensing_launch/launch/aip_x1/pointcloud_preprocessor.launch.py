e7f60a2c42c387dd8f937bc4b100e918f1b91d1c
Kosuke Takeuchi <kosuke.tnp@gmail.com>
2021-08-16 17:33:13 +0900

    Change ransac height thresh (#110) (#115)
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- sensing_launch/launch/aip_x1/pointcloud_preprocessor.launch.py
+++ sensing_launch/launch/aip_x1/pointcloud_preprocessor.launch.py
@@ -206,7 +206,7 @@ def launch_setup(context, *args, **kwargs):
             'min_points': 400,
             'min_inliers': 200,
             'max_iterations': 50,
-            'height_threshold': 0.12,
+            'height_threshold': 0.18,
             'plane_slope_threshold': 10.0,
             'voxel_size_x': 0.2,
             'voxel_size_y': 0.2,
